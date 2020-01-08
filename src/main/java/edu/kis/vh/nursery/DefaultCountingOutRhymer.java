228f5cb4f43a637957e4f7a5ac79db60d884b83b
Kamil Sykuła <kamil.sykula@automotivedev.pl>
2020-01-08 11:47:40 +0100

    Replace magic numbers with finals variables

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,10 +1,13 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
+    
+    public static final int FULL = 11;
+    public static final int EMPTY = -1;
+    public static final int SIZE = 12;
+    private int[] numbers = new int[SIZE];
 
-    private int[] numbers = new int[12];
-
-    public int total = -1;
+    public int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return numbers[total--];
     }
 }
